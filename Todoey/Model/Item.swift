d8915fe02ac848cb2b73cdc5115e9c4bf36f3a3b
Roberto Corchero <roberto.corchero@alu.uclm.es>
2019-07-08 21:46:05 +0200

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Roberto Corchero Aragón on 08/07/2019.
+//  Copyright © 2019 Roberto Corchero Aragón. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
